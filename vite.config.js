099bc8d85b476d6861ec39b948733e231e430ae9
Ray Ague <rayague03@gmail.com>
2025-10-16 14:12:46 +0200

    gestion de la mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -8,4 +21,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'dist', // ou 'public/build' si tu préfères
+    }
 });
